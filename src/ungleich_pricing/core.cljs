3b202898085f7cba3bb1d06c83cd83e7d6cad6ba
Munen Alain M. Lafon <munen@dispatched.ch>
2015-11-23 14:42:44 +0100

    include all hetzner prices

--- src/ungleich_pricing/core.cljs
+++ src/ungleich_pricing/core.cljs
@@ -3,42 +3,67 @@
 
 (enable-console-print!)
 
-(defonce app-state (atom {:count 1 }))
+(defonce app-state (atom {:cores 1
+                          :memory 5
+                          :hd 1
+                          :selected :hetzner}))
 
-(defn simple-slider [type]
+(defn log [type]
+  [:input {:type "input" :value (type @app-state) :size 2}])
+
+(defn slider [type]
   [:div
    [:span (str type)]
+   [log type]
    [:input {:type "range" 
-            :value (:count @app-state)
+            :value (type @app-state)
             :min 1
             :max 100
-            :on-change #(swap! app-state assoc :count (-> % .-target .-value))
+            :on-change #(swap! app-state assoc type (.-target.value %))
             }]])
-
-(defn log []
-  [:span "Logging: " (:count @app-state)])
 
 (defn calculate-price []
   (let [hetzner_base 10
         hetzner_core 10
         hetzner_memory 5
-        hetzner_hd 1]
-    (+ hetzner_base
-       hetzner_hd
-       hetzner_memory
-       (* (:count @app-state) hetzner_core))))
+        hetzner_hd 1
+        factor {:hetzner 1
+                :hetzner-raid6 1.2
+                :hetzner-glusterfs 1.4}]
+    (* ((:selected @app-state) factor)
+        (+ hetzner_base
+        (* (:hd @app-state) hetzner_hd)
+        (* (:memory @app-state) hetzner_memory)
+        (* (:cores @app-state) hetzner_core)))))
 
 (defn price []
   [:div
-    [:span "Price: " (calculate-price)]])
+   [:div "Price: " (int (calculate-price))]
+   [:div "Price (incl. vat): " (int (* 1.08 (calculate-price)))]
+   ])
 
 (defn main []
-  [:div
-   [:h2 "Ungleich Pricing"]
-   [log]
-   [simple-slider :cores]
-   [price]
-   ])
+  [:div{:class "container"}
+   [:div{:class "jumbotron"}
+    [:h2 "Ungleich Pricing"]
+    [:div
+     [:p [:input{:type "radio"
+                 :name "options"
+                 :value "hetzner"
+                 :on-click #(swap! app-state assoc :selected (keyword (-> % .-target.value)))}] " Hetzner"]
+     [:p [:input{:type "radio"
+                 :name "options"
+                 :value "hetzner-raid6"
+                 :on-click #(swap! app-state assoc :selected (keyword (-> % .-target.value)))}] "Hetzner Raid6"]
+     [:p [:input{:type "radio"
+                 :name "options"
+                 :value "hetzner-glusterfs"
+                 :on-click #(swap! app-state assoc :selected (keyword (-> % .-target.value)))}] "Hetzner GlusterFS" ]]
+    [slider :cores]
+    [slider :memory]
+    [slider :hd]
+    [price]
+    ]])
 
 
 (reagent/render-component [main]
@@ -48,5 +73,5 @@
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
-  ;; (swap! app-state update-in [:__figwheel_counter] inc)
+  ;; (swap! app-state update-in [:__figwheel_countser] inc)
 )
