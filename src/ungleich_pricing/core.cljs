8a79c2d1688ccc76f1733656bd9baf5c9cbfd6c8
Munen Alain M. Lafon <munen@dispatched.ch>
2015-12-02 09:26:15 +0100

    some bootstrap responsive styling

--- src/ungleich_pricing/core.cljs
+++ src/ungleich_pricing/core.cljs
@@ -1,5 +1,6 @@
 (ns ungleich-pricing.core
-  (:require [reagent.core :as reagent :refer [atom]]))
+  (:require [reagent.core :as reagent :refer [atom]]
+            [clojure.string :refer [capitalize]]))
 
 (enable-console-print!)
 
@@ -18,14 +19,13 @@
 
 (defn slider [type]
   [:div
-   [:span (str type)]
+   [:span.label.label-default (capitalize (name type))]
    [log type]
    [:input {:type "range" 
             :value (type @resources)
             :min 1
             :max 100
-            :on-change #(swap! resources assoc type (-> % .-target .-value))
-            }]])
+            :on-change #(swap! resources assoc type (-> % .-target .-value))}]])
 
 (defn calculate-price []
   (let [hetzner_base 10
@@ -41,43 +41,44 @@
           (* (:memory @resources) hetzner_memory)
           (* (:cores @resources) hetzner_core)))))
 
+(defn euro []
+  [:span.glyphicon.glyphicon-eur])
+
 (defn price []
-  [:div
-   [:div "Price: " (int (calculate-price))]
-   [:div "Price (incl. vat): " (int (* 1.08 (calculate-price)))]])
+  [:div.well
+   [:h5 "Price: " (int (calculate-price))
+    [euro]]
+
+   [:h5 "Price (incl. vat): " (int (* 1.08 (calculate-price)))
+    [euro]]])
 
 (defn switch-hosting [e]
   (reset! selected-hosting (keyword (-> e .-target .-value))))
 
 (defn hosting-option [short_name name]
-  [:p
-   [:input {:type "radio"
-            :name "options"
-            :value short_name
-            :checked (= short_name @selected-hosting)
-            :on-click #(switch-hosting %)}] name])
+  [:div.col-xs-4
+   [:h4
+    [:input {:type "radio"
+             :name "options"
+             :value short_name
+             :checked (= short_name @selected-hosting)
+             :on-click #(switch-hosting %)}] name]])
             
 (defn main []
   [:div{:class "container"}
    [:div{:class "jumbotron"}
     [:h2 "Ungleich Pricing"]
-    [:div
+    [:div.row
      [hosting-option :hetzner "Hetzner"]
      [hosting-option :hetzner-raid6 "Hetzner Raid6"]
      [hosting-option :hetzner-glusterfs "Hetzner GlusterFS"]]
-    [slider :cores]
-    [slider :memory]
-    [slider :hd]
-    [price]
+    [:div.row
+     [slider :cores]
+     [slider :memory]
+     [slider :hd]]
+    [:div.row
+     [price]]
     ]])
-
 
 (reagent/render-component [main]
                           (. js/document (getElementById "app")))
-
-
-(defn on-js-reload []
-  ;; optionally touch your resources to force rerendering depending on
-  ;; your application
-  ;; (swap! resources update-in [:__figwheel_countser] inc)
-)
